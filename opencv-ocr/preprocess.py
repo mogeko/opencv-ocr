71538f6164b5ffd38fbb64956d1e3e16406965ed
Mogeko <zhengjunyi@live.com>
2023-04-20 13:15:56 +0200

    feat: expose the pre-processed parameters

--- opencv-ocr/preprocess.py
+++ opencv-ocr/preprocess.py
@@ -1,11 +1,11 @@
 import cv2 as cv
 
 
-def preprocess(img):
+def preprocess(img, ksize=(3, 3), sigma=1, alpha=1.2, beta=0):
     # 去除噪声，使用高斯模糊
-    img = cv.GaussianBlur(img, (3, 3), 0)
+    img = cv.GaussianBlur(img, ksize=ksize, sigmaX=sigma)
     # 增强对比度
-    img = cv.convertScaleAbs(img, alpha=1.5, beta=0)
+    img = cv.convertScaleAbs(img, alpha=alpha, beta=beta)
     # 将图像转为灰度图像
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # 二值化处理
