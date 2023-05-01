0f281d4194e415c680531c3437a0e8b186904480
Mogeko <zhengjunyi@live.com>
2023-05-01 19:37:14 +0200

    refactor: cut characters through projection

---
+++ opencv-ocr/extract_proj.py
@@ -0,0 +1,88 @@
+import cv2 as cv
+import numpy as np
+from itertools import product
+
+
+def get_h_progection(img):
+    r, c = img.shape
+    h_progection = np.zeros(img.shape, np.uint8)
+    hrowsum = [0]*r
+    for i, j in product(range(r), range(c)):
+        if img[i, j] == 255:
+            hrowsum[i] += 1
+    for i in range(r):
+        for j in range(hrowsum[i]):
+            h_progection[i, j] = 255
+    return hrowsum
+
+
+def get_v_progection(img):
+    r, c = img.shape
+    v_progection = np.zeros(img.shape, np.uint8)
+    vcolsum = [0]*c
+    for i, j in product(range(r), range(c)):
+        if img[i, j] == 255:
+            vcolsum[j] += 1
+    for j in range(c):
+        for i in range(r-vcolsum[j], r):
+            v_progection[i, j] = 255
+    return vcolsum
+
+
+def extract_by_progection(img):
+    # 二值化
+    _, img_bin = cv.threshold(
+        img, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+
+    # 计算水平投影
+    h_proj = np.sum(img, axis=1)
+
+    # 根据水平投影分割行
+    rows, start = [], 0
+    for i in range(len(h_proj)):
+        if h_proj[i] == 0:
+            if i > start:
+                rows.append(img_bin[start:i, :])
+            start = i+1
+
+    # 计算垂直投影
+    v_proj = [np.sum(row, axis=0) for row in rows]
+
+    # 根据垂直投影分割字符
+    for i in range(len(rows)):
+        cols, start = [], 0
+        for j in range(len(v_proj[i])):
+            if v_proj[i][j] == 0:
+                if j > start:
+                    cols.append(rows[i][:, start:j])
+                start = j+1
+        rows[i] = cols
+
+    print([len(row) for row in rows])
+
+    # 为字符添加边框
+    for row in range(len(rows)):
+        for col in range(len(rows[row])):
+            rows[row][col] = cv.copyMakeBorder(
+                rows[row][col], 20, 20, 20, 20, cv.BORDER_CONSTANT, value=0)
+
+    return rows, [np.sum(row, axis=0) for row in h_proj]
+
+
+if __name__ == '__main__':
+    from preprocess import preprocess_image
+    from localize import localize_text
+
+    img = cv.imread('examples/simple_2.png')
+    img = preprocess_image(img)
+    cv.imshow('Original', img)
+    contours = localize_text(img)
+    imgs, p = extract_by_progection(img)
+
+    for i in range(len(imgs)):
+        for j in range(len(imgs[i])):
+            cv.imshow('Character {}-{}'.format(i, j), imgs[i][j])
+
+    cv.imwrite('examples/extracted.png', imgs[0][0])
+    cv.waitKey(0)
+    cv.destroyAllWindows()
